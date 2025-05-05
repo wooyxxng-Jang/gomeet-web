da75cf877724da8a05fbb9c00157674f72c8fc10
장우영 <113076752+wooyxxng-Jang@users.noreply.github.com>
2025-05-05 17:39:24 +0900

    고밋 도록 웹사이트 초안

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll Reveal 애니메이션
+ScrollReveal().reveal(".hero .container", {
+  delay: 200,
+  duration: 1000,
+  reset: true,
+});
+ScrollReveal().reveal(".card", { interval: 200, duration: 1000, reset: true });
+ScrollReveal().reveal(".about-content", {
+  delay: 300,
+  duration: 1000,
+  reset: true,
+});
+ScrollReveal().reveal(".footer", { delay: 500, duration: 1000, reset: true });
